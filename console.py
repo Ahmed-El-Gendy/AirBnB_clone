ebde180ddac09d39c0d3fc45dbd674d9bd4697bf
Ramyrashad127 <ramy41599@gmail.com>
2023-12-09 12:48:07 -0800

    new

--- console.py
+++ console.py
@@ -2,7 +2,17 @@
 """
 contains the entry point of the command interpreter
 """
+import re
 import cmd
+import json
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.review import Review
+from models.amenity import Amenity
+from models.place import Place
 
 
 class HBNBCommand(cmd.Cmd):
