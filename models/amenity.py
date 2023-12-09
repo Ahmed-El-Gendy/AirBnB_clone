7f31d41708c3300e9249da16a69aa8f940e5164c
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-12-09 08:12:25 -0800

    task

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" new class that inheret from basemodel class """
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ new the child of basemodel class """
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/user/bin/python3
+""" new class basemodel the parent class """
+
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ state child class of basemodel class """
+    name = ""
