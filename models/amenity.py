393dcbfe9f49167899d7b0af0cb5c78764e55eef
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-12-09 04:04:47 -0800

    task

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" new """
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ new """
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/user/bin/python3
+""" new """
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ state """
+    name = ""
