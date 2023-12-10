5d03578bd06a15aff9ef9dcf6cf8c4bdce70d6da
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-12-10 12:03:17 -0800

    task

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""test"""
+
+import models
+import unittest
+from models.base_model import BaseModel
+
+
+class TestState(unittest.TestCase):
+    """test"""
+
+    def test_name(any):
+        """test"""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
