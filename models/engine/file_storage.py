29d0c56ab97bf86ed8bcf21bc46ec93e08c1211d
Ramyrashad127 <ramy41599@gmail.com>
2023-12-10 01:18:16 -0800

    new

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -36,21 +36,19 @@ class FileStorage:
             json.dump(serialized, f)
 
     def reload(self):
-        """ deserializes the JSON file """
-        def reload(self):
     """Deserializes the JSON file."""
-    if not os.path.exists(FileStorage.__file_path):
-        return
-    try:
-        with open(self.__file_path, 'r', encoding="utf-8") as f:
-            data = json.load(f)
-            for value in data.values():
-                class_name = value["__class__"]
-                model_class = getattr(models, class_name)
-                self.new(model_class(**value))
-    except FileNotFoundError:
-        pass
-    except json.JSONDecodeError:
-        pass
-    except Exception as e:
-        pass
+        if not os.path.exists(FileStorage.__file_path):
+            return
+        try:
+            with open(self.__file_path, 'r', encoding="utf-8") as f:
+                data = json.load(f)
+                for value in data.values():
+                    class_name = value["__class__"]
+                    model_class = getattr(models, class_name)
+                    self.new(model_class(**value))
+        except FileNotFoundError:
+            pass
+        except json.JSONDecodeError:
+            pass
+        except Exception as e:
+            pass
