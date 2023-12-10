b91b996b6727da1e08a248ca0ff7e4486ff0ca91
Ramyrashad127 <ramy41599@gmail.com>
2023-12-10 03:24:00 -0800

    new

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" test filestorage """
+import os
+import models
+import unittest
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
